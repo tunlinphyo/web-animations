ab3eaa8e15afb10b7f4550871ef53ed2d29df6b0
tunlinphyo <uniqueof1990@gmail.com>
2023-06-17 22:48:17 +0900

    update scroll base typo

--- pages/scroll-base-animation/js/scroll-base.js
+++ pages/scroll-base-animation/js/scroll-base.js
@@ -1,67 +1,66 @@
 export default class ScrollBase {
   constructor(selector) {
-      this.elem = document.querySelector(selector)
-      this.handleScroll = this.handleScroll.bind(this)
-      this.handleResize = this.handleResize.bind(this)
-      this.subscribe()
+    this.elem = document.querySelector(selector);
+    this.handleScroll = this.handleScroll.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+    this.subscribe();
   }
 
   handleScroll() {
-      const scrollY = 
-      this.onScroll(window.scrollY)
+    this.onScroll(window.scrollY);
   }
 
   handleResize() {
-      this.getElementPositions()
-      this.onResize()
+    this.getElementPositions();
+    this.onResize();
   }
 
   onScroll(scrollY) {
-      console.log('SCROLL', scrollY)
+    console.log("SCROLL", scrollY);
   }
 
   onResize() {
-      console.log('CALCULATE')
+    console.log("CALCULATE");
   }
 
   percentage(num, end) {
-      return num / end * 100
+    return (num / end) * 100;
   }
 
   getElementPositions() {
-      const top = this.elem.parentElement.offsetTop
-      const height = this.elem.clientHeight
-      const parentHeight = this.elem.parentElement.clientHeight
+    const top = this.elem.parentElement.offsetTop;
+    const height = this.elem.clientHeight;
+    const parentHeight = this.elem.parentElement.clientHeight;
 
-      this.start = top - ((window.innerHeight - height) / 2)
-      this.end = this.start + parentHeight - height
-      this.range = this.end - this.start
+    this.start = top - (window.innerHeight - height) / 2;
+    this.end = this.start + parentHeight - height;
+    this.range = this.end - this.start;
   }
 
   mapRange(number, startRange1, endRange1, startRange2, endRange2) {
-      if (number < startRange1) {
-        number = startRange1;
-      } else if (number > endRange1) {
-        number = endRange1;
-      }
-      
-      const range1 = endRange1 - startRange1;
-      const range2 = endRange2 - startRange2;
-      
-      const mappedValue = (((number - startRange1) * range2) / range1) + startRange2;
-      
-      return mappedValue;
+    if (number < startRange1) {
+      number = startRange1;
+    } else if (number > endRange1) {
+      number = endRange1;
     }
 
+    const range1 = endRange1 - startRange1;
+    const range2 = endRange2 - startRange2;
+
+    const mappedValue =
+      ((number - startRange1) * range2) / range1 + startRange2;
+
+    return mappedValue;
+  }
+
   subscribe() {
-      this.getElementPositions()
-      this.onResize()
-      document.addEventListener('scroll', this.handleScroll)
-      window.addEventListener('resize', this.handleResize)
+    this.handleResize();
+    document.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("resize", this.handleResize);
   }
 
   unsubscribe() {
-      document.removeEventListener('scroll', this.handleScroll)
-      window.removeEventListener('resize', this.handleResize)
+    document.removeEventListener("scroll", this.handleScroll);
+    window.removeEventListener("resize", this.handleResize);
   }
 }
